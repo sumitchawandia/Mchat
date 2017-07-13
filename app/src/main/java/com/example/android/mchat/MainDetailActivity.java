c320d71065e96dc24acdd38b7be40e6dbbfeb651
sumitchawandia <calibersumit@gmail.com>
2017-07-14 03:04:44 +0530

    mchat

---
+++ app/src/main/java/com/example/android/mchat/MainDetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.mchat;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by sumit on 6/14/2017.
+ */
+
+public class MainDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.introduction_detail_activity);
+    }
+}
